0545d566022404ebe07ab09d79769a833eaf5322
Rick Eyre <rick.eyre@hotmail.com>
2014-03-24 15:29:41 -0400

    Add basic tests.

---
+++ tests/test.js
@@ -0,0 +1,85 @@
+var assert = Object.create(require("assert")),
+    path = require("path"),
+    fs = require("fs"),
+    vttFile = path.join(__dirname, "/basic.vtt"),
+    NodeVTT = require("../lib/node-vtt.js"),
+    nodeVTT = new NodeVTT();
+
+describe("basic tests", function(){
+
+  before(function(onDone) {
+    nodeVTT.init(onDone);
+  });
+
+  after(function() {
+    nodeVTT.shutdown();
+  });
+
+  it("api", function() {
+    ( "cues regions errors ready vtt phantom " +
+      "flush clear parse parseFile processFile " + 
+      "processParsedData" ).split(" ").forEach(function(key) {
+        assert.ok(key in nodeVTT, "NodeVTT should have the " + key + " property.");
+      });
+  });
+
+  it("parse", function(onDone) {
+    var vtt = fs.readFileSync(vttFile);
+    nodeVTT.parse(vtt, function(error) {
+      assert.ok(!error, "parse should succeed.");
+      assert.equal(nodeVTT.cues.length, 1, "We should have one cue.");
+      assert.equal(nodeVTT.regions.length, 1, "We should have one region.");
+      onDone();
+    });
+  });
+
+  it("flush", function(onDone) {
+    nodeVTT.flush(function() {
+      assert.equal(nodeVTT.cues.length, 2, "We should have two cues.");
+      assert.equal(nodeVTT.regions.length, 1, "We should have one region.");
+      onDone();
+    });
+  })
+
+  it("processParsedData", function(onDone) {
+    nodeVTT.processParsedData(function(error, data) {
+      assert.ok(!error, "processParsedData should succeed.");
+      assert.ok(data.length, 2, "Two elements should have been returned.");
+      onDone();
+    });
+  });
+
+  it("clear", function(onDone) {
+    nodeVTT.clear(function() {
+      assert.equal(nodeVTT.cues.length, 0, "We should have zero cues.");
+      assert.equal(nodeVTT.regions.length, 0, "We should have zero regions.");
+      onDone();
+    });
+  });
+
+  it("parseFile", function(onDone) {
+    nodeVTT.parseFile(vttFile, function(error) {
+      assert.ok(!error, "parseFile should succeed.");
+      assert.equal(nodeVTT.cues.length, 2, "We should have two cues.");
+      assert.equal(nodeVTT.regions.length, 1, "We should have one region.");
+      onDone();
+    });
+  });
+
+  it("second clear", function(onDone) {
+    nodeVTT.clear(function() {
+      assert.equal(nodeVTT.cues.length, 0, "We should have zero cues.");
+      assert.equal(nodeVTT.regions.length, 0, "We should have zero regions.");
+      onDone();
+    });
+  });
+
+  it("processFile", function(onDone) {
+    nodeVTT.processFile(vttFile, function(error, data) {
+      assert.ok(!error, "processParsedData should succeed.");
+      assert.ok(data.length, 2, "Two elements should have been returned.");
+      onDone();
+    });
+  });
+
+});
